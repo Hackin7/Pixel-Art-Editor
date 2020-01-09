06f595a92bab388a29f07a38e236e30aab2fbdc7
Terence Chan Zun Mun <zunmun@gmail.com>
2020-01-10 01:02:01 +0800

    Fix the sluggishness refresh bug

--- PlayOnLEDMatrix.py
+++ PlayOnLEDMatrix.py
@@ -2,13 +2,14 @@ import time
 import board
 import neopixel
 size=64
-pixels = neopixel.NeoPixel(board.D18, size)
+pixels = neopixel.NeoPixel(board.D18, size, auto_write=False)
 
 textIMG = {}
 
 def toShowFrame(matrixList):
     for i in range(size):
         pixels[i] = tuple(matrixList[i])
+    pixels.show()
 
 import time
 def toShowAnimation(animation):
@@ -16,5 +17,4 @@ def toShowAnimation(animation):
         toShowFrame(frame)
         #toShowFrame(frame)
         #toShowFrame(frame)
-        time.sleep(int(int(animation['timing'])/1000)) #in ms
-    
+        time.sleep((int(animation['timing'])/1000)) #in ms
